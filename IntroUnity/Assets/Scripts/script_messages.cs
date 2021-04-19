026b094d352d571e218c83308130294f7432230d
Orange-Spice <orange.spice@hotmail.com>
2021-04-19 23:31:27 +0200

    Project structure

---
+++ IntroUnity/Assets/Scripts/script_messages.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_messages : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hola Mundo");
+        //Debug.LogError("Esto es un log de error");
+        Debug.LogWarning("Esto es un log de Warning");
+    }
+}
